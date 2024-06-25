3547f38de845cda3f90b6003767e2f3c698d236c
HavocFox <duskydraws@gmail.com>
2024-06-24 21:19:17 -0400

    i think it still works...

---
+++ Fix look here/react2/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import UserProfile from './UserProfile'; // Make sure the path is correct
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+    <div className = "App">
+      <UserProfile />
+      </div>
+    </>
+    
+  )
+}
+
+export default App
---
+++ Fix look here/react2/src/UserProfile.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+class UserProfile extends Component {
+  constructor(props) {
+      super(props);
+      this.state = { 
+        name: 'Alex',
+        editMode: false,  // Add a flag for toggling between Edit and View modes
+        hobbies: ['Reading', 'Traveling', 'Gaming'],  // Example hobbies list
+      };
+      this.changeName = this.changeName.bind(this);
+      this.toggleEditMode = this.toggleEditMode.bind(this);
+  }
+
+  changeName() {
+      this.setState({ name: 'Charlie' });
+  }
+
+  toggleEditMode() {
+      this.setState(prevState => ({ editMode: !prevState.editMode }));
+  }
+
+  render() {
+      const { name, editMode, hobbies } = this.state;
+      return (
+          <div>
+              <h1>User Profile</h1>
+              <p>Name: {name}</p>
+              <button onClick={this.changeName}>Change Name</button>
+
+              <button onClick={this.toggleEditMode}>
+                  {editMode ? 'View' : 'Edit'}
+              </button>
+              
+              {editMode ? (
+                  <div>
+                      <h2>Edit Mode</h2>
+                  </div>
+              ) : (
+                  <div>
+                      <h2>View Mode</h2>
+                      <ul>
+                          {hobbies.map((hobby, index) => (
+                              <li key={index}>{hobby}</li>
+                          ))}
+                      </ul>
+                  </div>
+              )}
+          </div>
+      );
+  }
+}
+
+export default UserProfile;
